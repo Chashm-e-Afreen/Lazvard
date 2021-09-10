6e6a1630f62be0b88ede4dc518cb65547f886102
Chashm-e-Afreen <belta.infinita@yandex.com>
2021-09-10 08:52:06 +0100

    Remove Public

--- src/pages/about.js
+++ src/pages/about.js
@@ -35,7 +35,7 @@ const AboutPage = ({ data }, location) => {
             but I will try my best.
           </p>
           <p>I recently contemplated rewriting my metrical scansion engine for Urdu in Typescript as part of the second version
-            of my web application.
+            of my web application. 
             <h4>
               <strong>
                 <strong>Objectives</strong>
@@ -44,25 +44,25 @@ const AboutPage = ({ data }, location) => {
             <ul>
               <li>Pure Typescript implementation to remove the need for a backend.</li>
               <li>Experiment with newer UI/UX techniques.</li>
-              <li>Make a Single Page Application for a more fluid experience.</li>
+              <li>Make a Single Page Application using a JS framework for a more fluid experience.</li>
             </ul>
           </p>
           <h4>
-            <strong>
-              <strong>Result</strong>
-            </strong>
-          </h4>
-          <ul>
-            <li>The engine turned out fine. Slower than my C++ implementation but still reasonably fast.</li>
-            <li>Clean codebase that relies heavily on functional programming techniques.</li>
-            <li>Installable Progressive Web App that works offline on all platforms.</li>
-            <li>Fluid UI with Gatsby and React.</li>
-          </ul>
+              <strong>
+                <strong>Result</strong>
+              </strong>
+            </h4>
+            <ul>
+              <li>The engine turned out fine. Slower than my C++ implementation but still reasonably fast.</li>
+              <li>Clean codebase that relies heavily on functional programming techniques.</li>
+              <li>Installable Progressive Web App that works offline on all platforms.</li>
+              <li>Fluid UI with Gatsby and React.</li>
+            </ul>
+            
+            <p>Eventually I decided to embed the app into a somewhat multipurpose website.
+            </p>
 
-          <p>Eventually I decided to embed the app into a somewhat multipurpose website.
-          </p>
-
-          <h3>―― Muhammad Rehan Qureshi</h3>
+            <h3>―― Muhammad Rehan Qureshi</h3>
         </div>
       </article>
     </Layout>
@@ -96,5 +96,3 @@ export default props => (
     )}
   />
 )
-
-
